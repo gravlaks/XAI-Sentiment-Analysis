5a649f5af7d133aa812cb9649628a8407db3093b
Jonas Rødningen <jonasrod@stud.ntnu.no>
2021-03-03 11:33:09 +0100

    history plot and accuracy evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,23 +1,28 @@
-from sklearn.model_selection import cross_validate, StratifiedKFold
+import matplotlib.pyplot as plt
 
 
-# TODO just temp copied from Jonas' ML project.training.py
-def evaluate_model(X, y, model,  gpu_mode=False):
-    """
-    Evaulates the model using k-fold kross validation.
-    ---
-    returns two dataframes: 
-    1 => scores for all runs
-    2 => average scores
-    """
-    cv = StratifiedKFold(n_splits=NUM_FOLDS, random_state=SEED, shuffle=True)
-    N_JOBS = 1 if gpu_mode else -1
+def evaluate_model(model, X_test, y_test):
+    # The labels here assume we use accuracy for our model
+    score = model.evaluate(X_test, y_test, verbose=True)
+    print('Accuracy', score[1])
 
-    scores = cross_validate(model, X, y, scoring=[
-                            'f1_macro', 'f1_micro'], cv=cv, n_jobs=N_JOBS)
 
-    averages = {}
-    for key, val in scores.items():
-        averages[key] = val.mean()
+def plot_history(history):
+    # The labels here assume we use accuracy for our model
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
 
-    return pd.DataFrame(scores), averages
+    plt.title('Model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+
+    plt.title('Model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
