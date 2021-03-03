d3e059300ba0f20869dd5e3ada3d258b16d91599
Jonas Rødningen <jonasrod@stud.ntnu.no>
2021-03-03 11:15:49 +0100

    add function

--- model.py
+++ model.py
@@ -7,22 +7,27 @@ from keras.layers import Input, Embedding, Lambda, Bidirectional, LSTM, Dense, D
 from keras.models import Model
 import numpy as np
 
-# TODO: TEMP vars
+# TODO: TEMP vars, delete when pipeline is ready for it
 maxlen = 30
 vocab_size = 1000
 embedding_matrix = np.zeros((1000, 100))
 
-model = Sequential()
-model.add(Embedding(vocab_size, 100, weights=[
-    embedding_matrix], trainable=False, name='GloVe_Embedding'))
-model.add(LSTM(128, return_sequences=True))
-# model.add(LSTM(64, return_sequences=True))
-model.add(Dropout(0.1))
-model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
-# model.add(Dropout(0.05))
-# model.add(LSTM(16))
-model.add(Dense(1, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+def build_model_keras():
+    model = Sequential()
+    model.add(Embedding(vocab_size, 100, weights=[
+        embedding_matrix], trainable=False, name='GloVe_Embedding'))
+    model.add(LSTM(128, return_sequences=True))
+    # model.add(LSTM(64, return_sequences=True))
+    model.add(Dropout(0.1))
+    model.add(Dense(32, activation='relu',
+                    kernel_regularizer=regularizers.l2(0.01)))
+    # model.add(Dropout(0.05))
+    # model.add(LSTM(16))
+    model.add(Dense(1, activation='relu',
+                    kernel_regularizer=regularizers.l2(0.01)))
 
-print(model.summary())
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy', metrics=['acc'])
+
+    return model
