da1a37661caa5977b35c6b2352e339352d2901c2
Jonas Rødningen <jonasrod@stud.ntnu.no>
2021-03-10 15:08:38 +0100

    temp commit

--- model.py
+++ model.py
@@ -19,10 +19,10 @@ import tensorflow as tf
 from emb import get_keras_embeddings_layer
 
 
-class KerasTextClassifier(BaseEstimator, TransformerMixin):
+class KerasTextClassifier(BaseEstimator, TransformerMixin, ):
     '''Wrapper class for keras text classification models that takes raw text as input.'''
 
-    def __init__(self, glove, prepoc, max_words=30000, input_length=100, n_classes=3, epochs=10, batch_size=4000):
+    def __init__(self, glove, prepoc, tokenizer, max_words=30000, input_length=100, n_classes=3, epochs=10, batch_size=4000):
         self.glove = glove
         self.prepoc = prepoc
         self.input_length = input_length
@@ -30,8 +30,7 @@ class KerasTextClassifier(BaseEstimator, TransformerMixin):
         self.epochs = epochs
         self.bs = batch_size
         self.model = self._get_model()
-        self.tokenizer = Tokenizer(num_words=MAX_WORDS,
-                                   lower=True, split=' ', oov_token="UNK")
+        self.tokenizer = tokenizer
 
     def _get_model(self):
 
